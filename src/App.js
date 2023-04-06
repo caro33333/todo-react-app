a048992fa731613bf48db3e650f9f613a3c29657
hj <slip__@naver.com>
2023-04-06 11:22:16 +0900

    프론트엔드 16

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from "react";
 import Todo from "./Todo";
 import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core";
+import { Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 import './App.css';
+import { call, signout } from './service/ApiService';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [
-                { id: 0, title: "Todo 1", done: true },
-                { id: 1, title: "Todo 2", done: false },
-            ],
+            items: [],
+            loading: true,
         };
     }
 
     add = (item) => {
-        const thisItems = this.state.items;
-        item.id = "ID-" + thisItems.length;
-        item.done = false;
-        thisItems.push(item);
-        this.setState({ items: thisItems });
-        console.log("items:", this.state.items);
+        call("/todo","POST",item).then((response) =>
+            this.setState({items: response.data})
+        );
     }
 
     delete = (item) => {
-        const thisItems = this.state.items;
-        const newItems = thisItems.filter(e => e.id !== item.id);
-        this.setState({ items: newItems }, () => {
-            console.log("update items : ", this.state.items);
-        });
+        call("/todo","DELETE",item).then((response) =>
+        this.setState({items:response.data})
+        );
+    }
+    
+    update = (item) => {
+        call("/todo","PUT",item).then((response) =>
+        this.setState({items:response.data})
+        );
+    }
+
+    componentDidMount() {
+        call("/todo","GET",null).then((response) =>
+        this.setState({items:response.data, loading:false})
+        );
     }
 
     render() {
@@ -37,18 +43,48 @@ class App extends React.Component {
             <Paper style={{ margin: 16 }}>
                 <List>
                     {this.state.items.map((item, idx) => (
-                        <Todo item={item} key={item.id} delete={this.delete}/>
+                        <Todo item={item} key={item.id} delete={this.delete} update={this.update}/>
                     ))}
                 </List>
             </Paper>
         );
 
+    var navigationBar = (
+        <AppBar position="static">
+            <Toolbar>
+                <Grid justify="space-between" container>
+                    <Grid item>
+                        <Typography variant="h6">오늘의 할 일</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button color="inherit" onClick={signout}>logout
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
+
+    var todoListPage = (
+        <div>
+        {navigationBar}
+        <Container maxWidth="md">
+            <AddTodo add={this.add} />
+            <div className="TodoList">{todoItems}</div>
+        </Container>
+        </div>
+    );
+
+    var loadingPage = <h1>로딩중..</h1>
+    var content = loadingPage;
+
+    if(!this.state.loading) {
+        content = todoListPage;
+    }
+
     return(
             <div className = "App" >
-                <Container maxWidth="md">
-                    <AddTodo add={this.add} />
-                    <div className="TodoList">{todoItems}</div>
-                </Container>
+                {content}
             </div>
         );
     }
--- src/Todo.js
+++ src/Todo.js
@@ -7,6 +7,7 @@ class Todo extends React.Component {
         super(props);
         this.state = {item : props.item, readOnly: true};
         this.delete = props.delete;
+        this.update = props.update;
     }
 
     deleteEventHandler = () => {
@@ -22,7 +23,8 @@ class Todo extends React.Component {
 
     enterKeyEventHandler = (e) => {
         if(e.key === "Enter") {
-            this.setState({readOnly: true})
+            this.setState({readOnly: true});
+            this.update(this.state.item);
         }
     }
 
@@ -36,7 +38,8 @@ class Todo extends React.Component {
         console.log("check box event call");
         const thisItem = this.state.item;
         thisItem.done = thisItem.done ? false: true;
-        this.setState({item: thisItem});
+        this.setState({readOnly:true});
+        this.update(this.state.item);
     }
 
     render() {
